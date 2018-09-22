31b8c82c091cae0500355bac424ca4f41fbf0d20
Kevin Dunetz <kevindunetz@kevindunetz-macbookpro.roam.corp.google.com>
2018-09-22 10:36:08 -0400

    lakjsdf

--- updateVersion.sh
+++ updateVersion.sh
@@ -1,2 +1,2 @@
 #cat src/main/resources/static/index.html | sed 's/VERSION/${env.BUILD_NUMBER}/g' > src/main/resources/static/index.html
-sed -i "s/VERSION/${env.BUILD_NUMBER}/g"  src/main/resources/static/index.html
+sed -i 's/VERSION/${env.BUILD_NUMBER}/g' src/main/resources/static/index.html
